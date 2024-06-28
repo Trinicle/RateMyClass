41a4215a9548683f0e863a2b52d1f133263acace
Hunter Freeland <hunter.freeland@parkplacetech.com>
2024-06-28 17:02:06 -0400

    18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,31 +11,24 @@ import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { HomeNavbarComponent } from './navbar/home-navbar/home-navbar.component';
 import { GeneralNavbarComponent } from './navbar/general-navbar/general-navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { SearchComponent } from './home/search/search.component';
 import { UniversityComponent } from './university/university.component';
 import { ChartComponent } from './university/chart/chart.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    HomeComponent,
-    HomeNavbarComponent,
-    GeneralNavbarComponent,
-    SearchComponent,
-    UniversityComponent,
-    ChartComponent,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    SharedModule,
-    ReactiveFormsModule,
-  ],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        SignupComponent,
+        HomeComponent,
+        HomeNavbarComponent,
+        GeneralNavbarComponent,
+        SearchComponent,
+        UniversityComponent,
+        ChartComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
