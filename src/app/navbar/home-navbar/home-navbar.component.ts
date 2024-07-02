9635a59ef9b62326f48dc8f1da52f2d997dc2c26
Hunter Freeland <hunter.freeland@parkplacetech.com>
2024-07-02 17:21:34 -0400

    styling

--- src/app/navbar/home-navbar/home-navbar.component.ts
+++ src/app/navbar/home-navbar/home-navbar.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'home-navbar',
-    templateUrl: './home-navbar.component.html',
-    styleUrls: ['./home-navbar.component.scss'],
-    standalone: true,
-    imports: [RouterLink]
+  selector: 'home-navbar',
+  templateUrl: './home-navbar.component.html',
+  styleUrls: ['./home-navbar.component.scss'],
+  standalone: true,
+  imports: [RouterLink],
 })
 export class HomeNavbarComponent {
   isHome: boolean = true;
-
 }
