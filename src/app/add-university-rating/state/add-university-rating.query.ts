595b3cf7b1f58861c9122768f899014d749eaae7
Hunter <hunter.freeland2003@gmail.com>
2024-07-05 16:29:40 -0400

    no need

--- src/app/add-university-rating/state/add-university-rating.query.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Query } from '@datorama/akita';
-
-@Injectable({ providedIn: 'root' })
-export class UniversityDetailsQuery extends Query<> {
-  constructor(protected override store: ) {
-    super(store);
-  }
-}
