569e19a7d894ee7e0a44d264d39248b2f8b70992
Hunter Freeland <hunter.freeland@parkplacetech.com>
2024-07-11 16:45:26 -0400

    more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { UniversityComponent } from './university/university.component';
 import { AddUniversityRatingComponent } from './add-university-rating/add-university-rating.component';
 import { CoursesComponent } from './courses/courses.component';
@@ -39,16 +37,7 @@ const routes: Routes = [
   //   component: AddUniversityRatingComponent,
   //   title: 'Add University Rating',
   // },
-  {
-    path: 'login',
-    component: LoginComponent,
-    title: 'Login',
-  },
-  {
-    path: 'signup',
-    component: SignupComponent,
-    title: 'Signup',
-  },
+
   {
     path: '',
     redirectTo: '/home',
